ec6a7cf08e53073f5d131f1de12a86c7215bff8b
dgladowski <dawid.gladowski@lo1.sandomierz.pl>
2018-09-11 14:19:11 +0200

    nie wiem

---
+++ 2a/python/wejsce_wyjscie.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main(args):
+    a = input("Podaj liczbę 1: ")
+
+    b = input("Podaj liczbę 2: ")
+    print(b)
+    print(a)
+    return 0    
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
