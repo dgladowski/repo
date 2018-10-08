8d5e44c73b36c705f806426b91dbb7e233e68a4c
dgladowski <gladowski3@gmail.com>
2018-10-08 23:22:55 +0200

    pd

--- 2a/python/rysunek_trojkat.py
+++ 2a/python/rysunek_trojkat.py
@@ -2,26 +2,24 @@
 # -*- coding: utf-8 -*-
 #
 #  trojkat.py
-#  
 
 def trojkat(a, b, c):
     """
-    Funkcja przyjmuje trzy liczby - długości boków trójkąta
-    Funkcja sprawdza, czy da sie z nich zbudować trójkąt
-    Funkcja zwraca True jeśli się da, False w przeciwnym razie
+    funkcja przyjmuje trzy liczby - długości boków
+            sprawdza, czy da się z nich zbudować trójkąt
+            zwraca True jeśli się da, False w przeciwnym razie 
     """
-
+    
     wynik = False
     
     if a + b > c and a + c > b and b + c > a:
-        wynik = True 
-    
+        wynik = True
     
     return wynik
 
 def main(args):
     assert(trojkat(3, 6, 8) == True)
-    assert(trojkat(3, 5, 9) == False)        
+    assert(trojkat(3, 5, 9) == False)
     return 0
 
 if __name__ == '__main__':
